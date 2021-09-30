5fff8c1490d7b1af738c93fd6d1edba513892e14
Matt Nelson-White <mnelsonwhite@gmail.com>
2021-09-30 19:52:33 +1000

    Initial Commit

---
+++ Reversii/ReversiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiiApp.swift
+//  Reversii
+//
+//  Created by Matthew Nelson-White on 30/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ReversiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
